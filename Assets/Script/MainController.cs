a73e393ce2e915177d0ba3143335739123fb6770
iyokurot <01kayochin17neko@gmail.com>
2019-10-04 08:56:19 +0900

    メイン画面構想

---
+++ Assets/Script/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MainController : MonoBehaviour{
+    [SerializeField]
+    GameObject demoMovie;
+    string videoUrl="file://C:/Users/03yuk/Desktop/製作物提出用/PromotionImage.mp4";
+    // Start is called before the first frame update
+    void Start()
+    {
+        demoMovie.GetComponent<VideoPlayer>().url=videoUrl;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
